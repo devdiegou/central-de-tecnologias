3b4ecc5267b43a1f6f5cf5296de4411c117a27d1
Diego Alves <devdiegou@gmail.com>
2023-10-26 18:49:49 -0300

    feitas algumas correções

--- src/Routes/RoutesMain.jsx
+++ src/Routes/RoutesMain.jsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-import { DashboardPage } from "../Pages/Dashboard/dashboard";
-import { LoginPage } from "../pages/Login/login";
-import { RegisterPage } from "../Pages/Register/register";
 import { useState } from "react";
+import { LoginPage } from "../pages/Login/login";
+import { RegisterPage } from "../pages/Register/register";
+import { DashboardPage } from "../pages/Dashboard/dashboard";
 
 export const RoutesMain = () => {
   const [user, setUser] = useState({});
 
   return (
     <Routes>
-      <Route path="/" element={<LoginPage setUser={setUser}/>} />
+      <Route path="/" element={<LoginPage setUser={setUser}/>}/>
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/dashboard" element={<DashboardPage user={user} setUser={setUser} />} />
+      <Route path="/dashboard" element={<DashboardPage user={user} setUser={setUser}/>}/>
     </Routes>
   );
 };
