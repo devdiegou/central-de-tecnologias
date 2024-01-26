ca047bb9428b82462f4fb575e43a94983cd622d6
Diego Alves <devdiegou@gmail.com>
2024-01-26 17:45:52 -0300

    commit de salvamento

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -3,14 +3,14 @@ import { FormLogin } from "../../components/FormLogin/formLogin";
 import style from "./login.module.scss";
 import { UserContext } from "../../providers/userContext";
 
-export const LoginPage = () => {
+const LoginPage = () => {
   const { setUser } = useContext(UserContext);
 
   return (
     <>
       <div className="global">
         <div className={style.login__container}>
-          <h2 className={style.title} >Code Hub</h2>
+          <h2 className={style.title}>Code Hub</h2>
 
           <FormLogin setUser={setUser} />
         </div>
@@ -18,3 +18,5 @@ export const LoginPage = () => {
     </>
   );
 };
+
+export default LoginPage;
